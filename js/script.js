c667df57efd73fdacf630951f6890bb6c19c2f2f
Valentin Himself <valentinhimself@gmail.com>
2022-02-01 10:24:53 -0700

    added logic to play one round through future ui

--- js/script.js
+++ js/script.js
@@ -1,91 +1,80 @@
-function game() {
-    /* contains all functions needed to run the game to reduce global scope pollution,
-    sets counters for player and computer, 
-    loops 5 rounds by initializing playerPlay() function,
-    logs the score into the console,
-    return a player-to-computer score; */
+let rock = document.querySelector('.rock').addEventListener('click', playerPlay);
+let paper = document.querySelector('.paper').addEventListener('click', playerPlay);
+let scissors = document.querySelector('.scissors').addEventListener('click', playerPlay);
+//create a global variable that will be accessible between functions;
+let globalPlayerSelection;
 
-    function computerPlay () {
 
-        /*function selects a random number between 0 and 2 and
-        assigns each to number "Rock", "Paper" and "Scissors" respectively */
+function playerPlay (e) {
+    //get the class of the selection: rock, paper scissors.
+    globalPlayerSelection = e.target.className; //.innerText .id .tagName are some others that are available;
+    //make the first letter Capital
+    globalPlayerSelection = globalPlayerSelection[0].toUpperCase()+globalPlayerSelection.slice(1).toLowerCase();
+    console.log(`Player selection is ${globalPlayerSelection}`)
+    //initiate the functon for the round
+    playRound();
+    //selection = null;
+}
 
-        let num = Math.floor(Math.random() * 3);
-        if (num==0) {
-            num = "Rock";
-        } else if (num==1) {
-            num = "Paper";
-        } else {
-            num = "Scissors";
-        }
-        return num;
+function playRound () {
+    let playerSelection = globalPlayerSelection;
+    //call the function for computer selection
+    let computerSelection = computerPlay();
+
+    /*function itself compares playerSelection and computerSelection to determine the winner */
+
+    let str = " ";
+
+    if (playerSelection==computerSelection){
+        str = "Draw";
+        console.log(str);
     }
-
-    function playerPlay () {
-        /* function asks the player to make a selection, 
-        capitalizes the first letter and ensures that all other letters are lower case
-        and loops if player makes an invalid choice */
-        let selection = prompt ("Rock, Paper, Scissors?");
-        selection = selection[0].toUpperCase()+selection.slice(1).toLowerCase();
-        while (true) {
-            if (selection == "Rock" || selection == "Paper" || selection == "Scissors") {
-                break; }
-            else {
-                selection = prompt ("Invalid choice. Type Rock, Paper or Scissors.");
-                selection = selection[0].toUpperCase()+selection.slice(1).toLowerCase();}
-            }
-        return selection }
-
-
-    function playRound () {
-        let playerSelection = playerPlay();
-        let computerSelection = computerPlay();
-
-        /*function compares playerSelection and computerSelection to determine the winner */
-
-        let str = " ";
-
-        if (playerSelection==computerSelection){
-            str = "Draw";
-            console.log(str);
-        }
-        else if ((playerSelection == "Paper" && computerSelection == "Rock")
-        || (playerSelection == "Scissors" && computerSelection == "Paper")
-        || (playerSelection == "Rock" && computerSelection == "Scissors")) {
-            str = `You win: ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
-            playerCounter += 1; 
-            console.log(str);
-        }
-        else {
-            str = `You lose: ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
-            computerCounter +=1; 
-            console.log(str);
-            
-        }
-        return str;
-    }
-    let playerCounter = 0;
-    let computerCounter = 0;
-    for (i=0; i<5; i++) {
-        playRound();
-    }
-    let score = `the score is ${playerCounter} to ${computerCounter}`
-    console.log(score)
-    globalScore = [playerCounter, computerCounter];
-    return globalScore;
-}   
-
-function getWinner () {
-    if (globalScore[0]<globalScore[1]) {
-        console.log("The machines reign supreme")
-    }
-    else if (globalScore[0]>globalScore[1]) {
-        console.log("Humanity gets to see another day")
+    else if ((playerSelection == "Paper" && computerSelection == "Rock")
+    || (playerSelection == "Scissors" && computerSelection == "Paper")
+    || (playerSelection == "Rock" && computerSelection == "Scissors")) {
+        str = `You win: ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
+        playerCounter += 1; 
+        console.log(str);
     }
     else {
-        console.log ("A perpetual war ensues")
+        str = `You lose: ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
+        computerCounter +=1; 
+        console.log(str);
+        
     }
+    return str;
 }
-let globalScore;
-let startGame = game ();
-let winner = getWinner ();
+
+function computerPlay () {
+
+    /*function selects a random number between 0 and 2 and
+    assigns each to number "Rock", "Paper" and "Scissors" respectively */
+
+    let num = Math.floor(Math.random() * 3);
+    if (num==0) {
+        num = "Rock";
+    } else if (num==1) {
+        num = "Paper";
+    } else {
+        num = "Scissors";
+    }
+    console.log(`Computer selection is ${num}`)
+    return num;
+}
+
+let playerCounter = 0;
+let computerCounter = 0;
+let globalScore = [playerCounter, computerCounter];
+
+function getWinner () {
+globalScore = [playerCounter, computerCounter];
+if (globalScore[0]<globalScore[1]) {
+    console.log("The machines reign supreme")
+}
+else if (globalScore[0]>globalScore[1]) {
+    console.log("Humanity gets to see another day")
+}
+else {
+    console.log ("A perpetual war ensues")
+}
+}
