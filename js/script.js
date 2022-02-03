bc5a96ed10ed7ba1e1dfac6a839df828afac5d97
Valentin Himself <valentinhimself@gmail.com>
2022-02-03 11:21:13 -0700

    repositioned the buttons, changed the overall styling, added a reset button and added winner declaration

--- js/script.js
+++ js/script.js
@@ -1,23 +1,58 @@
-let rock = document.querySelector('.rock').addEventListener('click', playerPlay);
-let paper = document.querySelector('.paper').addEventListener('click', playerPlay);
-let scissors = document.querySelector('.scissors').addEventListener('click', playerPlay);
-
+const rock = document.querySelector('.rock').addEventListener('click', playerPlay);
+const paper = document.querySelector('.paper').addEventListener('click', playerPlay);
+const scissors = document.querySelector('.scissors').addEventListener('click', playerPlay);
 //create a variable to make changes to Round counter;
 const roundsDiv = document.querySelector('.round');
 let roundsCount = 2; //start with Round 2, since Round 1 is already set in HTML.
-
-
 //create a global variable for player's choice that will be accessible between functions;
 let globalPlayerSelection;
-
 let computerCounter = 0;
 let playerCounter = 0;
-
 //create variables to make changes to wins:
 const playerWin = document.querySelector('.player');
 const computerWin = document.querySelector('.computer');
+const winner_p = document.getElementById('winner');
+const winner_span = document.createElement('span');
+//const gameReset = document.getElementById('reset-game').addEventListener('click', resetGame);
+const gameReset = document.getElementById('reset-game').addEventListener('click', () => window.location.reload());
 
-let globalScore = [playerCounter, computerCounter];
+
+
+function playerPlay (e) {
+    setRoundCounter();
+    //get the class of the selection: rock, paper scissors.
+    globalPlayerSelection = e.target.className; //.innerText .id .tagName are some others that are available;
+    e.target.classList.add("scaled");
+    setTimeout(() => e.target.classList.remove("scaled"),200);
+   
+    //make the first letter Capital
+    globalPlayerSelection = globalPlayerSelection[0].toUpperCase()+globalPlayerSelection.slice(1).toLowerCase();
+    console.log(`Player selection is ${globalPlayerSelection}`)
+    //initiate the functon for the round
+    playRound();    
+}
+
+function playRound () {
+    let playerSelection = globalPlayerSelection;
+    //call the function for computer selection
+    let computerSelection = computerPlay();
+    /*function itself compares playerSelection and computerSelection to determine the winner */
+    let str = " ";
+    if (playerSelection==computerSelection){
+        str = "Draw";
+        console.log(str);
+    }
+    else if ((playerSelection == "Paper" && computerSelection == "Rock")
+    || (playerSelection == "Scissors" && computerSelection == "Paper")
+    || (playerSelection == "Rock" && computerSelection == "Scissors")) {
+        setPlayerWins();
+    }
+    else {
+        setComputerWins ();  
+    }
+    addPlayerImage ();
+    return str;
+}
 
 function addPlayerImage () {
     let imageContainer = document.querySelector('.player-choices');
@@ -31,51 +66,8 @@ function addPlayerImage () {
     else {
         image.src="./images/scissors.png"
     }
-    image.setAttribute('style', "display:block; width: 75px; height: auto; position: relative; right: 25px;")
+    image.setAttribute('style', "display:block; width: 60px; height: auto; position: relative; right: 25px;")
     imageContainer.append(image)
-}
-function playerPlay (e) {
-    setRoundCounter();
-    //get the class of the selection: rock, paper scissors.
-    globalPlayerSelection = e.target.className; //.innerText .id .tagName are some others that are available;
- //   e.target.classList.add("scaled");
-   
-    //make the first letter Capital
-    globalPlayerSelection = globalPlayerSelection[0].toUpperCase()+globalPlayerSelection.slice(1).toLowerCase();
-    console.log(`Player selection is ${globalPlayerSelection}`)
-    //initiate the functon for the round
-    playRound();
-    addPlayerImage ();
-    
-}
-
-function playRound () {
-    let playerSelection = globalPlayerSelection;
-    //call the function for computer selection
-    let computerSelection = computerPlay();
-
-    /*function itself compares playerSelection and computerSelection to determine the winner */
-
-    let str = " ";
-
-    if (playerSelection==computerSelection){
-        str = "Draw";
-        console.log(str);
-    }
-    else if ((playerSelection == "Paper" && computerSelection == "Rock")
-    || (playerSelection == "Scissors" && computerSelection == "Paper")
-    || (playerSelection == "Rock" && computerSelection == "Scissors")) {
-        str = `You win: ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
-        setPlayerWins();
-        console.log(str);
-    }
-    else {
-        str = `You lose: ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
-        setComputerWins ();
-        console.log(str);
-        
-    }
-    return str;
 }
 
 function computerPlay () {
@@ -104,12 +96,11 @@ function computerPlay () {
         else {
             image.src="./images/scissors.png"
         }
-        image.setAttribute('style', "display:block; width: 75px; height: auto; position: relative; right: 25px;")
+        image.setAttribute('style', "display:block; width: 60px; height: auto; position: relative; right: 25px;")
         imageContainer.append(image)
     }
     addComputerImage();
     return num;
-
 }
 
 function setRoundCounter () {
@@ -119,23 +110,28 @@ function setRoundCounter () {
 
 function setPlayerWins () {
     ++playerCounter;
-    playerWin.textContent = `Player: ${playerCounter}`
+    playerWin.textContent = `Player: ${playerCounter}`;
+    if (playerCounter===5 && computerCounter<playerCounter ) {declarePlayerWinner()}
+    if (playerCounter>5) { resetGame() }
 }
 
 function setComputerWins () {
     ++computerCounter;
     computerWin.textContent = `Computer: ${computerCounter}`
+    if (computerCounter===5 && computerCounter>playerCounter ) {declareCompWinner()}
+    if (computerCounter>5) { resetGame() }
 }
 
-function getWinner () {
-globalScore = [playerCounter, computerCounter];
-if (globalScore[0]<globalScore[1]) {
-    console.log("The machines reign supreme")
+function declareCompWinner () {
+    winner_span.textContent="the machine"
+    winner_p.append(winner_span)
 }
-else if (globalScore[0]>globalScore[1]) {
-    console.log("Humanity gets to see another day")
+
+function declarePlayerWinner () {
+    winner_span.textContent="the player"
+    winner_p.append(winner_span)
 }
-else {
-    console.log ("A perpetual war ensues")
-}
-}
+
+function resetGame(){
+    window.location.reload();
+} 
